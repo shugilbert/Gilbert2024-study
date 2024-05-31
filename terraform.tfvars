bddb1f57e84b5562eb8d48a80d161d0740e5f59c
shu gilbert <shugilberts@gmail.com>
2024-05-30 21:46:12 -0500

    second

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+
+ami_id ="ami-0d191299f2822b1fa"
+instance_type = "t2.medium"
+av_zonea = "us-east-1a"
+av_zonec = "us-east-1c"
+#availability_zone = "us-east-1b"
+tag = "assign-server"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "av_zonea" {
+  
+}
+variable "av_zonec" {
+  
+}
+
+#variable "availability_zone" {
+  
+
+variable "tag"{
+
+}
